3a6504b08132f5fccb81a177ef64e5f41858caad
VladimirGnezdilov <vovikgnep@gmail.com>
2025-12-18 18:57:42 +0500

    Create vladimir 37.1.py

---
+++ lessons/lesson 37 (18.12). Практическая работа с функциями/tasks/1/vladimir 37.1.py
@@ -0,0 +1,23 @@
+def rectangle_info(width, height):
+    P=(width+height)*2
+    S=height*width
+    return S, P
+print(rectangle_info(10,12)
+
+
+
+def is_adult(age):
+    if age>=18:
+        return True
+    else:
+        return False
+print(is_adult(20))
+
+
+
+def safe_div(a, b):
+    if b==0:
+        return None
+    else:
+        return a/b
+print(safe_div(20,10))
